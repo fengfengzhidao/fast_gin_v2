1c1d6f7cd4e464000f5b4f92604fd728ae16f3f1
fengfeng <2663456523@qq.com>
2025-01-15 23:39:58 +0800

    logrus接入

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"fast_gin/core"
 	"fast_gin/flags"
 	"fast_gin/global"
-	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	core.InitLogger()
 	flags.Run()
 	global.Config = core.ReadConfig()
-	global.Config.DB.Port = 3307
-	fmt.Println(global.Config.DB)
+
+	logrus.Debugf("你好")
+	logrus.Infof("你好")
+	logrus.Warnf("你好")
+	logrus.Errorf("你好")
 }
