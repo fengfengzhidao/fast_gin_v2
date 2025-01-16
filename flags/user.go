071532f62c004f3aab09646c358a2b08250ac51b
fengfeng <2663456523@qq.com>
2025-01-16 20:12:57 +0800

    密码加密与用户创建

--- flags/user.go
+++ flags/user.go
@@ -1,8 +1,11 @@
 package flags
 
 import (
+	"fast_gin/global"
 	"fast_gin/models"
+	"fast_gin/utils/pwd"
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
 	"os"
 )
@@ -25,6 +28,13 @@ func (User) Create() {
 	fmt.Println("请输入用户名")
 	fmt.Scanln(&user.Username)
 
+	var u models.UserModel
+	err = global.DB.Take(&u, "username = ?", user.Username).Error
+	if err == nil {
+		fmt.Println("用户名已存在")
+		return
+	}
+
 	fmt.Println("请输入密码")
 	password, err := terminal.ReadPassword(int(os.Stdin.Fd())) // 读取用户输入的密码
 	if err != nil {
@@ -42,11 +52,29 @@ func (User) Create() {
 		return
 	}
 
-	fmt.Println(user.RoleID)
-	fmt.Println(user.Username)
-	fmt.Println(string(password))
+	hashPwd := pwd.GenerateFromPassword(string(password))
+	err = global.DB.Create(&models.UserModel{
+		Username: user.Username,
+		Password: hashPwd,
+		RoleID:   user.RoleID,
+	}).Error
+	if err != nil {
+		logrus.Errorf("用户创建失败 %s", err)
+		return
+	}
+	logrus.Infof("用户创建成功")
 
 }
 func (User) List() {
-
+	var userList []models.UserModel
+	global.DB.Order("created_at desc").Limit(10).Find(&userList)
+	for _, model := range userList {
+		fmt.Printf("用户id：%d  用户名：%s 用户昵称：%s 用户角色：%d 创建时间：%s\n",
+			model.ID,
+			model.Username,
+			model.Nickname,
+			model.RoleID,
+			model.CreatedAt.Format("2006-01-02 15:04:05"),
+		)
+	}
 }
