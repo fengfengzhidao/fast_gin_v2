0033d44025f7ba99c3c36924d3c54d6113a298b0
fengfeng <2663456523@qq.com>
2025-01-16 20:55:46 +0800

    jwt

--- config/enter.go
+++ config/enter.go
@@ -4,4 +4,5 @@ type Config struct {
 	DB     DB     `yaml:"db"`
 	Redis  Redis  `yaml:"redis"`
 	System System `yaml:"system"`
+	Jwt    Jwt    `yaml:"jwt"`
 }
---
+++ testdata/1.生成token.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fast_gin/core"
+	"fast_gin/flags"
+	"fast_gin/global"
+	"fast_gin/utils/jwts"
+	"fmt"
+)
+
+func main() {
+	core.InitLogger()
+	flags.Parse()
+	global.Config = core.ReadConfig()
+	token, err := jwts.SetToken(1)
+	fmt.Println(token, err)
+	claims, err := jwts.CheckToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlzcyI6ImZlbmdmZW5nIiwiZXhwIjoxNzM3MDYwNzY2fQ.MQ-uiLjU0UArChYUPx2EjqSWZYkpvNRdhk26LwaOjo1")
+	fmt.Println(claims, err)
+}
