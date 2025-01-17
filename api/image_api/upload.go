8a9c7dd024585d683b1022cce9592896cca84ba4
fengfeng <fengfengzhidao@126.com>
2025-01-17 20:15:32 +0800

    图片上传-判断重复问题

--- api/image_api/upload.go
+++ api/image_api/upload.go
@@ -3,8 +3,12 @@ package image_api
 import (
 	"fast_gin/global"
 	"fast_gin/utils/find"
+	"fast_gin/utils/md5"
+	"fast_gin/utils/random"
 	"fast_gin/utils/res"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
@@ -38,9 +42,31 @@ func (ImageApi) UploadView(c *gin.Context) {
 	}
 
 	// 处理文件名重复
-
+	// uploads/images/xx.jpg
+	// uploads/images/xx_7hf.jpg
 	fp := path.Join("uploads", global.Config.Upload.Dir, fileHeader.Filename)
+	for {
+		_, err1 := os.Stat(fp)
+		if os.IsNotExist(err1) {
+			break
+		}
+		// 文件存在
+		// 算上传的图片和本身的图片是不是一样的，如果是一样的，那就直接返回之前的地址
+		uploadFile, _ := fileHeader.Open()
+		oldFile, _ := os.Open(fp)
 
+		uploadFileHash := md5.MD5WithFile(uploadFile)
+		oldFileHash := md5.MD5WithFile(oldFile)
+		if uploadFileHash == oldFileHash {
+			// 上传的图片，名称和内容都是一样的
+			res.Ok("/"+fp, "上传成功", c)
+			return
+		}
+		// 上传的图片，名称是一样的，但是内容不一样
+		fileNameNotExt := strings.TrimSuffix(fileHeader.Filename, ext)
+		newFileName := fmt.Sprintf("%s_%s%s", fileNameNotExt, random.RandStr(3), ext)
+		fp = path.Join("uploads", global.Config.Upload.Dir, newFileName)
+	}
 	c.SaveUploadedFile(fileHeader, fp)
 
 	res.Ok("/"+fp, "上传成功", c)
