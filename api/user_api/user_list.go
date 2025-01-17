1d034637822a79f4502123a88a53bb778f680d30
fengfeng <fengfengzhidao@126.com>
2025-01-17 23:50:43 +0800

    用户列表

--- api/user_api/user_list.go
+++ api/user_api/user_list.go
@@ -1,13 +1,28 @@
 package user_api
 
 import (
+	"fast_gin/global"
 	"fast_gin/middleware"
+	"fast_gin/models"
 	"fast_gin/utils/res"
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func (UserApi) UserListView(c *gin.Context) {
-	fmt.Println(middleware.GetAuth(c))
-	res.OkWithData("用户列表", c)
+	var cr = middleware.GetBind[models.PageInfo](c)
+	var list = make([]models.UserModel, 0)
+	query := global.DB.Where("")
+
+	if cr.Key != "" {
+		query.Where("username like ?", "%"+cr.Key+"%")
+	}
+
+	offset := (cr.Page - 1) * cr.Limit
+
+	global.DB.Where(query).Limit(cr.Limit).Offset(offset).Order(cr.Order).Find(&list)
+
+	var count int64
+	global.DB.Where(query).Count(&count)
+
+	res.OkWithList(list, count, c)
 }
--- routers/user_router.go
+++ routers/user_router.go
@@ -4,12 +4,22 @@ import (
 	"fast_gin/api"
 	"fast_gin/api/user_api"
 	"fast_gin/middleware"
+	"fast_gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRouter(g *gin.RouterGroup) {
 	app := api.App.UserApi
-	g.POST("users/login", middleware.LimitMiddleware(2), middleware.BindJsonMiddleware[user_api.LoginRequest], app.LoginView)
-	g.GET("users", middleware.LimitMiddleware(10), middleware.AdminMiddleware, app.UserListView)
-	g.POST("users/logout", middleware.AuthMiddleware, app.LogoutView)
+	g.POST("users/login",
+		middleware.LimitMiddleware(2),
+		middleware.BindJsonMiddleware[user_api.LoginRequest],
+		app.LoginView)
+	g.GET("users",
+		middleware.LimitMiddleware(10),
+		middleware.AdminMiddleware,
+		middleware.BindQueryMiddleware[models.PageInfo],
+		app.UserListView)
+	g.POST("users/logout",
+		middleware.AuthMiddleware,
+		app.LogoutView)
 }
