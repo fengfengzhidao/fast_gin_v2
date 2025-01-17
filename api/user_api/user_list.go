833204a4bd4d8044b11b466644e19e790fdb3eb7
fengfeng <fengfengzhidao@126.com>
2025-01-18 00:09:53 +0800

    通用列表查询

--- api/user_api/user_list.go
+++ api/user_api/user_list.go
@@ -1,28 +1,20 @@
 package user_api
 
 import (
-	"fast_gin/global"
 	"fast_gin/middleware"
 	"fast_gin/models"
+	"fast_gin/service/common"
 	"fast_gin/utils/res"
 	"github.com/gin-gonic/gin"
 )
 
 func (UserApi) UserListView(c *gin.Context) {
 	var cr = middleware.GetBind[models.PageInfo](c)
-	var list = make([]models.UserModel, 0)
-	query := global.DB.Where("")
 
-	if cr.Key != "" {
-		query.Where("username like ?", "%"+cr.Key+"%")
-	}
-
-	offset := (cr.Page - 1) * cr.Limit
-
-	global.DB.Where(query).Limit(cr.Limit).Offset(offset).Order(cr.Order).Find(&list)
-
-	var count int64
-	global.DB.Where(query).Count(&count)
-
+	list, count, _ := common.QueryList(models.UserModel{}, common.QueryOption{
+		PageInfo: cr,
+		Likes:    []string{"username", "nickname"},
+		Debug:    true,
+	})
 	res.OkWithList(list, count, c)
 }
